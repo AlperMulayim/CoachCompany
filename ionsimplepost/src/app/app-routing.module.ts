dd11b084ebe78a8f8bce452610ce3df5c16dc8a2
AlperMulayim <alper.mulayim48@gmail.com>
2019-07-06 22:06:25 +0300

    post-home , new-post and news tab components created

--- ionsimplepost/src/app/app-routing.module.ts
+++ ionsimplepost/src/app/app-routing.module.ts
@@ -4,6 +4,9 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'post-home', loadChildren: './post-home/post-home.module#PostHomePageModule' },
+  { path: 'new-post', loadChildren: './new-post/new-post.module#NewPostPageModule' },
+  { path: 'news', loadChildren: './news/news.module#NewsPageModule' },
 ];
 
 @NgModule({
---
+++ ionsimplepost/src/app/post-home/post-home.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post-home',
+  templateUrl: './post-home.page.html',
+  styleUrls: ['./post-home.page.scss'],
+})
+export class PostHomePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
