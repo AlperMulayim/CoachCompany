9a924eb26c13951ae1c85252e2d13757bf1196a7
AlperMulayim <alper.mulayim48@gmail.com>
2019-08-26 22:04:44 +0300

    [COACH_UI] basic structure added to UI ( tabs shop and home )

--- uicoachmanager/src/app/app-routing.module.ts
+++ uicoachmanager/src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { HomePage } from './home/home.page';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: '', redirectTo: 'tabs/home', pathMatch: 'full' },
+  { path: 'tabs', loadChildren: './tabs/tabs.module#TabsPageModule' }
 ];
 
 @NgModule({
--- uicoachmanager/src/app/app.module.ts
+++ uicoachmanager/src/app/app.module.ts
@@ -8,11 +8,13 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { ShopPageModule } from './shop/shop.module';
+import { HomePageModule } from './home/home.module';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule,AppRoutingModule,HomePageModule,ShopPageModule,IonicModule.forRoot()],
   providers: [
     StatusBar,
     SplashScreen,
